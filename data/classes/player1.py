0569cd6d350957cf9b05af38d7df1b4ad13c18f0
Hayden Rome <46658679+hrome13@users.noreply.github.com>
2022-12-29 17:43:42 -0600

    Fix the calls to the move methods from Player

--- data/classes/player1.py
+++ data/classes/player1.py
@@ -8,19 +8,19 @@ class Player1(player.Player):
     def move(self, keys):
         # check if the right arrow key is pressed
         if keys[pygame.K_RIGHT]:
-            player.move_right()
+            self.move_right()
 
         # check if the left arrow key is pressed
         if keys[pygame.K_LEFT]:
-            player.move_left()
+            self.move_left()
 
         # check if the up arrow key is pressed
         if keys[pygame.K_UP]:
-            player.move_up()
+            self.move_up()
 
         # check if the down arrow key is pressed
         if keys[pygame.K_DOWN]:
-            player.move_down()
+            self.move_down()
 
     def took_shot(self, keys):
         return keys[pygame.K_SPACE]
--- data/classes/player2.py
+++ data/classes/player2.py
@@ -8,19 +8,19 @@ class Player2(player.Player):
     def move(self, keys):
         # check if the `d` key is pressed
         if keys[pygame.K_d]:
-            player.move_right()
+            self.move_right()
 
         # check if the `a` key is pressed
         if keys[pygame.K_a]:
-            player.move_left()
+            self.move_left()
 
         # check if the `w` key is pressed
         if keys[pygame.K_w]:
-            player.move_up()
+            self.move_up()
 
         # check if the `s` key is pressed
         if keys[pygame.K_s]:
-            player.move_down()
+            self.move_down()
 
     def took_shot(self, keys):
         return keys[pygame.K_LSHIFT]
