2f08bba95dd7f1fb3331978c33a6e6c3569a82f0
Hayden Rome <46658679+hrome13@users.noreply.github.com>
2022-12-29 17:34:58 -0600

    Add getters and setters for Projectile class

--- data/classes/projectile.py
+++ data/classes/projectile.py
@@ -2,14 +2,62 @@ import pygame
 
 class Projectile:
     def __init__(self, pos, width, height, speed):
-        self.pos = pos
-        self.left = pos[0]
-        self.right = pos[0] + width
-        self.top = pos[1]
-        self.bottom = pos[1] + height
-        self.speed = speed
+        self._pos = pos
+        self._left = pos[0]
+        self._right = pos[0] + width
+        self._top = pos[1]
+        self._bottom = pos[1] + height
+        self._speed = speed
+
+    @property
+    def pos(self):
+        return self._pos
+
+    @property
+    def left(self):
+        return self._left
+
+    @property
+    def right(self):
+        return self._right
+
+    @property
+    def top(self):
+        return self._top
+
+    @property
+    def bottom(self):
+        return self._bottom
+
+    @property
+    def speed(self):
+        return self._speed
+
+    @pos.setter
+    def pos(self, pos):
+        self._pos = pos
+
+    @left.setter
+    def left(self, left):
+        self._left = left
+
+    @right.setter
+    def right(self, right):
+        self._right = right
+
+    @top.setter
+    def top(self, top):
+        self._top = top
+
+    @bottom.setter
+    def bottom(self, bottom):
+        self._bottom = bottom
+
+    @speed.setter
+    def speed(self, speed):
+        self._speed = speed
 
     def move(self):
-        self.pos[1] -= self.speed
-        self.top -= self.speed
-        self.bottom -= self.speed
+        self.pos = [self.pos()[0], self.pos()[1] - self.speed()]
+        self.top = self.top() - self.speed()
+        self.bottom = self.bottom() - self.speed()
