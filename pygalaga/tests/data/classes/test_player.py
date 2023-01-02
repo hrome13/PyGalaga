10779b0f3d3e416cf8077bd967fc7d8d7c91ef3c
Hayden Rome <46658679+hrome13@users.noreply.github.com>
2023-01-02 11:22:10 -0600

    Create Player test class

---
+++ pygalaga/tests/data/classes/test_player.py
@@ -0,0 +1,15 @@
+import unittest
+from pygalaga.data.classes.player import Player
+
+class TestPlayer(unittest.TestCase):
+    def test_move_left():
+        pass
+
+    def test_move_right():
+        pass
+
+    def test_move_up():
+        pass
+
+    def test_move_down():
+        pass
