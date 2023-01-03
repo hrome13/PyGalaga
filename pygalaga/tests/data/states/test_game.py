d9b1b3f54f7af912981b24ed5953beaa9f8e442a
Hayden Rome <46658679+hrome13@users.noreply.github.com>
2023-01-02 21:12:10 -0600

    Set up test class for game.py

---
+++ pygalaga/tests/data/states/test_game.py
@@ -0,0 +1,21 @@
+import unittest
+from pygalaga.data.states.game import collides,  play_game
+
+class TestGame(unittest.TestCase):
+    def test_collides(self):
+        pass
+
+    def test_play_game_one_player_win(self):
+        pass
+
+    def test_play_game_one_player_loss(self):
+        pass
+
+    def test_play_game_two_player_win(self):
+        pass
+
+    def test_play_game_two_player_loss(self):
+        pass
+
+    def test_play_game_wrong_num_players(self):
+        pass
