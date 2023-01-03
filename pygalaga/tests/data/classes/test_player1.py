8c132bb33d08ce54bed270396349fea1e1733192
Hayden Rome <46658679+hrome13@users.noreply.github.com>
2023-01-02 19:52:55 -0600

    Implement tests for Player1.move()

--- pygalaga/tests/data/classes/test_player1.py
+++ pygalaga/tests/data/classes/test_player1.py
@@ -1,4 +1,5 @@
 import unittest
+import pygame
 from pygalaga.data.classes.player1 import Player1
 
 class TestPlayer1(unittest.TestCase):
@@ -23,41 +24,158 @@ class TestPlayer1(unittest.TestCase):
         - Space-bar is pressed
     """
     
-    def test_move_left():
+    def test_move_left(self):
+        pos = [20,20]
+        width, height = 5, 5
+        speed = 2
+        player1 = Player1(pos, width, height, speed)
+        keys = {pygame.K_LEFT: True}
+        player1.move(keys)
+
+        self.assertEqual(player1.pos, [18, 20])
+        self.assertEqual(player1.left, 18)
+        self.assertEqual(player1.right, 23)
+        self.assertEqual(player1.top, 20)
+        self.assertEqual(player1.bottom, 25)
+
+    def test_move_right(self):
+        pos = [20,20]
+        width, height = 5, 5
+        speed = 2
+        player1 = Player1(pos, width, height, speed)
+        keys = {pygame.K_RIGHT: True}
+        player1.move(keys)
+
+        self.assertEqual(player1.pos, [22, 20])
+        self.assertEqual(player1.left, 22)
+        self.assertEqual(player1.right, 27)
+        self.assertEqual(player1.top, 20)
+        self.assertEqual(player1.bottom, 25)
+
+    def test_move_up(self):
+        pos = [20,20]
+        width, height = 5, 5
+        speed = 2
+        player1 = Player1(pos, width, height, speed)
+        keys = {pygame.K_UP: True}
+        player1.move(keys)
+
+        self.assertEqual(player1.pos, [20, 18])
+        self.assertEqual(player1.left, 20)
+        self.assertEqual(player1.right, 25)
+        self.assertEqual(player1.top, 18)
+        self.assertEqual(player1.bottom, 23)
+
+    def test_move_down(self):
+        pos = [20,20]
+        width, height = 5, 5
+        speed = 2
+        player1 = Player1(pos, width, height, speed)
+        keys = {pygame.K_DOWN: True}
+        player1.move(keys)
+
+        self.assertEqual(player1.pos, [20, 22])
+        self.assertEqual(player1.left, 20)
+        self.assertEqual(player1.right, 25)
+        self.assertEqual(player1.top, 22)
+        self.assertEqual(player1.bottom, 27)
+
+    def test_move_no_keys_pressed(self):
+        pos = [20,20]
+        width, height = 5, 5
+        speed = 2
+        player1 = Player1(pos, width, height, speed)
+        keys = {}
+        player1.move(keys)
+
+        self.assertEqual(player1.pos, [20, 20])
+        self.assertEqual(player1.left, 20)
+        self.assertEqual(player1.right, 25)
+        self.assertEqual(player1.top, 20)
+        self.assertEqual(player1.bottom, 25)
+
+    def test_move_other_keys_pressed(self):
+        pos = [20,20]
+        width, height = 5, 5
+        speed = 2
+        player1 = Player1(pos, width, height, speed)
+        keys = {pygame.K_w: True}
+        player1.move(keys)
+
+        self.assertEqual(player1.pos, [20, 20])
+        self.assertEqual(player1.left, 20)
+        self.assertEqual(player1.right, 25)
+        self.assertEqual(player1.top, 20)
+        self.assertEqual(player1.bottom, 25)
+
+    def test_move_two_opposite_directions(self):
+        pos = [20,20]
+        width, height = 5, 5
+        speed = 2
+        player1 = Player1(pos, width, height, speed)
+        keys = {pygame.K_UP: True,
+                pygame.K_DOWN: True}
+        player1.move(keys)
+
+        self.assertEqual(player1.pos, [20, 20])
+        self.assertEqual(player1.left, 20)
+        self.assertEqual(player1.right, 25)
+        self.assertEqual(player1.top, 20)
+        self.assertEqual(player1.bottom, 25)
+
+    def test_move_two_perpendicular_directions(self):
+        pos = [20,20]
+        width, height = 5, 5
+        speed = 2
+        player1 = Player1(pos, width, height, speed)
+        keys = {pygame.K_DOWN: True,
+                pygame.K_LEFT: True}
+        player1.move(keys)
+
+        self.assertEqual(player1.pos, [18, 22])
+        self.assertEqual(player1.left, 18)
+        self.assertEqual(player1.right, 23)
+        self.assertEqual(player1.top, 22)
+        self.assertEqual(player1.bottom, 27)
+
+    def test_move_three_directions(self):
+        pos = [20,20]
+        width, height = 5, 5
+        speed = 2
+        player1 = Player1(pos, width, height, speed)
+        keys = {pygame.K_DOWN: True,
+                pygame.K_LEFT: True,
+                pygame.K_RIGHT: True}
+        player1.move(keys)
+
+        self.assertEqual(player1.pos, [20, 22])
+        self.assertEqual(player1.left, 20)
+        self.assertEqual(player1.right, 25)
+        self.assertEqual(player1.top, 22)
+        self.assertEqual(player1.bottom, 27)
+
+    def test_move_all_directions(self):
+        pos = [20,20]
+        width, height = 5, 5
+        speed = 2
+        player1 = Player1(pos, width, height, speed)
+        keys = {pygame.K_UP: True,
+                pygame.K_DOWN: True,
+                pygame.K_LEFT: True,
+                pygame.K_RIGHT: True}
+        player1.move(keys)
+
+        self.assertEqual(player1.pos, [20, 20])
+        self.assertEqual(player1.left, 20)
+        self.assertEqual(player1.right, 25)
+        self.assertEqual(player1.top, 20)
+        self.assertEqual(player1.bottom, 25)
+
+    def test_took_shot_no_keys_pressed(self):
         pass
 
-    def test_move_right():
+    def test_took_shot_other_keys_pressed(self):
         pass
 
-    def test_move_up():
-        pass
-
-    def test_move_down():
-        pass
-
-    def test_move_no_keys_pressed():
-        pass
-
-    def test_move_other_keys_pressed():
-        pass
-
-    def test_move_two_opposite_directions():
-        pass
-
-    def test_move_two_perpendicular_directions():
-        pass
-
-    def test_move_three_directions():
-        pass
-
-    def test_move_all_directions():
-        pass
-
-    def test_took_shot_no_keys_pressed():
-        pass
-
-    def test_took_shot_other_keys_pressed():
-        pass
-
-    def test_took_shot_spacebar_pressed():
+    def test_took_shot_spacebar_pressed(self):
         pass
