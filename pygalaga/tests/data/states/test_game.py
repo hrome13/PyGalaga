d1ef49b8afc04bcc0fb152a7e46cbf9190dc92c8
Hayden Rome <46658679+hrome13@users.noreply.github.com>
2023-01-03 21:58:59 -0600

    Implement tests for collides()

--- pygalaga/tests/data/states/test_game.py
+++ pygalaga/tests/data/states/test_game.py
@@ -1,9 +1,103 @@
 import unittest
+from pygalaga.data.classes.player import Player
 from pygalaga.data.states.game import collides,  play_game
 
 class TestGame(unittest.TestCase):
-    def test_collides(self):
-        pass
+    def test_collides_1_all_in_2_x(self):
+        pos1 = [10, 10]
+        pos2 = [11, 12]
+        width1, height1 = 10, 10
+        width2, height2 = 3, 7
+        speed = 1
+        obj1 = Player(pos1, width1, height1, speed)
+        obj2 = Player(pos2, width2, height2, speed)
+
+        result = collides(obj1, obj2)
+        self.assertTrue(result)
+
+    def test_collides_1_partly_in_2_x(self):
+        pos1 = [10, 10]
+        pos2 = [9, 12]
+        width1, height1 = 10, 10
+        width2, height2 = 3, 7
+        speed = 1
+        obj1 = Player(pos1, width1, height1, speed)
+        obj2 = Player(pos2, width2, height2, speed)
+
+        result = collides(obj1, obj2)
+        self.assertTrue(result)
+
+    def test_collides_1_all_in_2_y(self):
+        pos1 = [10, 10]
+        pos2 = [12, 11]
+        width1, height1 = 10, 10
+        width2, height2 = 7, 3
+        speed = 1
+        obj1 = Player(pos1, width1, height1, speed)
+        obj2 = Player(pos2, width2, height2, speed)
+
+        result = collides(obj1, obj2)
+        self.assertTrue(result)
+
+    def test_collides_1_partly_in_2_y(self):
+        pos1 = [10, 10]
+        pos2 = [12, 9]
+        width1, height1 = 10, 10
+        width2, height2 = 7, 3
+        speed = 1
+        obj1 = Player(pos1, width1, height1, speed)
+        obj2 = Player(pos2, width2, height2, speed)
+
+        result = collides(obj1, obj2)
+        self.assertTrue(result)
+
+    def test_collides_2_all_in_1_x(self):
+        pos1 = [10, 10]
+        pos2 = [11, 12]
+        width1, height1 = 10, 10
+        width2, height2 = 3, 7
+        speed = 1
+        obj1 = Player(pos1, width1, height1, speed)
+        obj2 = Player(pos2, width2, height2, speed)
+
+        result = collides(obj2, obj1)
+        self.assertTrue(result)
+
+    def test_collides_2_partly_in_1_x(self):
+        pos1 = [10, 10]
+        pos2 = [9, 12]
+        width1, height1 = 10, 10
+        width2, height2 = 3, 7
+        speed = 1
+        obj1 = Player(pos1, width1, height1, speed)
+        obj2 = Player(pos2, width2, height2, speed)
+
+        result = collides(obj2, obj1)
+        self.assertTrue(result)
+
+    def test_collides_2_all_in_1_y(self):
+        pos1 = [10, 10]
+        pos2 = [12, 11]
+        width1, height1 = 10, 10
+        width2, height2 = 7, 3
+        speed = 1
+        obj1 = Player(pos1, width1, height1, speed)
+        obj2 = Player(pos2, width2, height2, speed)
+
+        result = collides(obj2, obj1)
+        self.assertTrue(result)
+
+    def test_collides_2_partly_in_1_y(self):
+        pos1 = [10, 10]
+        pos2 = [12, 9]
+        width1, height1 = 10, 10
+        width2, height2 = 7, 3
+        speed = 1
+        obj1 = Player(pos1, width1, height1, speed)
+        obj2 = Player(pos2, width2, height2, speed)
+
+        result = collides(obj2, obj1)
+        self.assertTrue(result)
 
     def test_play_game_one_player_win(self):
         pass
